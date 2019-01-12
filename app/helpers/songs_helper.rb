0848ce2479743588553a9d7755a5a5e6cd0223f8
kev112 <werewaswas@live.com>
2019-01-11 22:03:48 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,7 @@
 module SongsHelper
-
+    def artist_select(song)
+        song.artists.each do |artist| 
+            "<input type='dropdown' name='artist' value=#{artist.id}>"
+        end
+    end
 end
